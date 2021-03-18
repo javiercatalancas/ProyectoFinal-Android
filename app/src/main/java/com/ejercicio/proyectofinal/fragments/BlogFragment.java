374e9e7a738b0f21ab92a74bb3f1a4fe39d4ba02
Javier Catalán <74968085+javiercatalancas@users.noreply.github.com>
2021-03-18 14:12:33 +0100

    18032021
    seguir con el gallery

--- app/src/main/java/com/ejercicio/proyectofinal/fragments/BlogFragment.java
+++ app/src/main/java/com/ejercicio/proyectofinal/fragments/BlogFragment.java
@@ -40,14 +40,49 @@ public class BlogFragment extends Fragment {
         entrada_array.add(new Entrada("VIRTUOSO", "a", "16/03/2021", "Nivel Avanzado", "Conviértete en un fabuloso guitarrista gracias a nuestro fabuloso método basado en los mejores clickbaits de Youtube. \n" +
                 "Conocerás todos los secretos de la técnica de los mejores \"Room-guitarist\" de Youtube. Tocarás como nadie sin salir de tu habitación.\n" +
                 "Olvídate del miedo escénico y de salir de tediosas giras donde no comerás bien y dormirás en un catre.\n" +
-                "Tus vértebras lo agradecerán! Todo ello por un módico precio de 55 €/mes.", "Orelar", "a", "a"));
-        entrada_array.add(new Entrada("GUITARRISTA", "c", "16/03/2021", "Nivel Básico", "Comienza a tocar la guitarra con este curso", "Orelar", "a", "a"));
-        entrada_array.add(new Entrada("PROFESIONAL", "e", "16/03/2021", "Nivel Intermedio", "Comienza a tocar la guitarra con este curso", "Orelar", "a", "a"));
-        entrada_array.add(new Entrada("NIÑOS", "f", "16/03/2021", "Nivel Básico", "Comienza a tocar la guitarra con este curso", "Orelar", "a", "a"));
-        entrada_array.add(new Entrada("SUPER PACK", "d", "16/03/2021", "Nivel Intermedio", "Comienza a tocar la guitarra con este curso", "Orelar", "a", "a"));
-        entrada_array.add(new Entrada("SESSION MUSICIAN", "b", "16/03/2021", "Nivel Avanzado", "Comienza a tocar la guitarra con este curso", "Orelar", "a", "a"));
-        entrada_array.add(new Entrada("BASS PLAYER", "k", "16/03/2021", "Nivel Básico", "Comienza a tocar el bajo con este curso", "Orelar", "a", "a"));
-        entrada_array.add(new Entrada("GUITARRA ACÚSTICA", "n", "16/03/2021", "Nivel Básico", "Comienza a tocar la guitarra con este curso", "Orelar", "a", "a"));
+                "Tus vértebras lo agradecerán! Todo ello por un módico precio de 55 €/mes.", "¿Imaginas dedicarte profesionalmente a la música? Mejor cambia de idea...\n" +
+                "Si todavía sigues leyendo esto es que eres un verdadero apasionado. Cómprate una guitarra y comienza a aprender todo lo necesario para ser un gran profesional en el futuro.\n" +
+                "Tenemos una gran biblioteca de recursos, métodos, programaciones y demás material necesario para que aprendas de la forma más cómoda y personalizada posible.\n" +
+                "Soñar es gratis, pero cuantas más vueltas le des, peor. Lánzate ya a través de BEST ONLINE GUITAR LESSONS y antes de que te quieras dar cuenta, estarás tocando un gran concierto en tu garito favorito.", "j", "j"));
+        entrada_array.add(new Entrada("GUITARRISTA", "c", "16/03/2021", "Nivel Básico", "Conviértete en un fabuloso guitarrista gracias a nuestro fabuloso método basado en los mejores clickbaits de Youtube. \n" +
+                "Conocerás todos los secretos de la técnica de los mejores \"Room-guitarist\" de Youtube. Tocarás como nadie sin salir de tu habitación. \n" +
+                "Olvídate del miedo escénico y de salir de tediosas giras donde no comerás bien y dormirás en un catre.\n" +
+                "Tus vértebras lo agradecerán! Todo ello por un módico precio de 55 €/mes.", "¿Imaginas dedicarte profesionalmente a la música? Mejor cambia de idea...\n" +
+                "Si todavía sigues leyendo esto es que eres un verdadero apasionado. Cómprate una guitarra y comienza a aprender todo lo necesario para ser un gran profesional en el futuro.\n" +
+                "Tenemos una gran biblioteca de recursos, métodos, programaciones y demás material necesario para que aprendas de la forma más cómoda y personalizada posible.\n" +
+                "Soñar es gratis, pero cuantas más vueltas le des, peor. Lánzate ya a través de BEST ONLINE GUITAR LESSONS y antes de que te quieras dar cuenta, estarás tocando un gran concierto en tu garito favorito.", "a", "c"));
+        entrada_array.add(new Entrada("PROFESIONAL", "e", "16/03/2021", "Nivel Intermedio", "Conviértete en un fabuloso guitarrista gracias a nuestro fabuloso método basado en los mejores clickbaits de Youtube. \n" +
+                "Conocerás todos los secretos de la técnica de los mejores \"Room-guitarist\" de Youtube. Tocarás como nadie sin salir de tu habitación.\n" +
+                "Olvídate del miedo escénico y de salir de tediosas giras donde no comerás bien y dormirás en un catre.\n" +
+                "Tus vértebras lo agradecerán! Todo ello por un módico precio de 55 €/mes.", "¿Imaginas dedicarte profesionalmente a la música? Mejor cambia de idea...\n" +
+                "Si todavía sigues leyendo esto es que eres un verdadero apasionado. Cómprate una guitarra y comienza a aprender todo lo necesario para ser un gran profesional en el futuro.\n" +
+                "Tenemos una gran biblioteca de recursos, métodos, programaciones y demás material necesario para que aprendas de la forma más cómoda y personalizada posible.\n" +
+                "Soñar es gratis, pero cuantas más vueltas le des, peor. Lánzate ya a través de BEST ONLINE GUITAR LESSONS y antes de que te quieras dar cuenta, estarás tocando un gran concierto en tu garito favorito.", "n", "n"));
+        entrada_array.add(new Entrada("NIÑOS", "f", "16/03/2021", "Nivel Básico", "Conviértete en un fabuloso guitarrista gracias a nuestro fabuloso método basado en los mejores clickbaits de Youtube. Conocerás todos los secretos de la técnica de los mejores \\\"Room-guitarist\\\" de Youtube. Tocarás como nadie sin salir de tu habitación. \n" +
+                "Olvídate del miedo escénico y de salir de tediosas giras donde no comerás bien y dormirás en un catre. Tus vértebras lo agradecerán! Todo ello por un módico precio de 55 €/mes.", "¿Imaginas dedicarte profesionalmente a la música? Mejor cambia de idea...\n" +
+                "Si todavía sigues leyendo esto es que eres un verdadero apasionado. Cómprate una guitarra y comienza a aprender todo lo necesario para ser un gran profesional en el futuro.\n" +
+                "Tenemos una gran biblioteca de recursos, métodos, programaciones y demás material necesario para que aprendas de la forma más cómoda y personalizada posible.\n" +
+                "Soñar es gratis, pero cuantas más vueltas le des, peor. Lánzate ya a través de BEST ONLINE GUITAR LESSONS y antes de que te quieras dar cuenta, estarás tocando un gran concierto en tu garito favorito.", "d", "d"));
+        entrada_array.add(new Entrada("SUPER PACK", "d", "16/03/2021", "Nivel Intermedio", "Conviértete en un fabuloso guitarrista gracias a nuestro fabuloso método basado en los mejores clickbaits de Youtube. Conocerás todos los secretos de la técnica de los mejores \\\"Room-guitarist\\\" de Youtube. Tocarás como nadie sin salir de tu habitación. \n" +
+                "Olvídate del miedo escénico y de salir de tediosas giras donde no comerás bien y dormirás en un catre. Tus vértebras lo agradecerán! Todo ello por un módico precio de 55 €/mes.", "¿Imaginas dedicarte profesionalmente a la música? Mejor cambia de idea...\n" +
+                "Si todavía sigues leyendo esto es que eres un verdadero apasionado. Cómprate una guitarra y comienza a aprender todo lo necesario para ser un gran profesional en el futuro.\n" +
+                "Tenemos una gran biblioteca de recursos, métodos, programaciones y demás material necesario para que aprendas de la forma más cómoda y personalizada posible.\n" +
+                "Soñar es gratis, pero cuantas más vueltas le des, peor. Lánzate ya a través de BEST ONLINE GUITAR LESSONS y antes de que te quieras dar cuenta, estarás tocando un gran concierto en tu garito favorito.", "g", "g"));
+        entrada_array.add(new Entrada("SESSION MUSICIAN", "b", "16/03/2021", "Nivel Avanzado", "Conviértete en un fabuloso guitarrista gracias a nuestro fabuloso método basado en los mejores clickbaits de Youtube. Conocerás todos los secretos de la técnica de los mejores \\\"Room-guitarist\\\" de Youtube. Tocarás como nadie sin salir de tu habitación. \n" +
+                "Olvídate del miedo escénico y de salir de tediosas giras donde no comerás bien y dormirás en un catre. Tus vértebras lo agradecerán! Todo ello por un módico precio de 55 €/mes.", "¿Imaginas dedicarte profesionalmente a la música? Mejor cambia de idea...\n" +
+                "Si todavía sigues leyendo esto es que eres un verdadero apasionado. Cómprate una guitarra y comienza a aprender todo lo necesario para ser un gran profesional en el futuro.\n" +
+                "Tenemos una gran biblioteca de recursos, métodos, programaciones y demás material necesario para que aprendas de la forma más cómoda y personalizada posible.\n" +
+                "Soñar es gratis, pero cuantas más vueltas le des, peor. Lánzate ya a través de BEST ONLINE GUITAR LESSONS y antes de que te quieras dar cuenta, estarás tocando un gran concierto en tu garito favorito.", "k", "k"));
+        entrada_array.add(new Entrada("BASS PLAYER", "k", "16/03/2021", "Nivel Básico", "Conviértete en un fabuloso guitarrista gracias a nuestro fabuloso método basado en los mejores clickbaits de Youtube. Conocerás todos los secretos de la técnica de los mejores \\\"Room-guitarist\\\" de Youtube. Tocarás como nadie sin salir de tu habitación. \n" +
+                "Olvídate del miedo escénico y de salir de tediosas giras donde no comerás bien y dormirás en un catre. Tus vértebras lo agradecerán! Todo ello por un módico precio de 55 €/mes.", "¿Imaginas dedicarte profesionalmente a la música? Mejor cambia de idea...\n" +
+                "Si todavía sigues leyendo esto es que eres un verdadero apasionado. Cómprate una guitarra y comienza a aprender todo lo necesario para ser un gran profesional en el futuro.\n" +
+                "Tenemos una gran biblioteca de recursos, métodos, programaciones y demás material necesario para que aprendas de la forma más cómoda y personalizada posible.\n" +
+                "Soñar es gratis, pero cuantas más vueltas le des, peor. Lánzate ya a través de BEST ONLINE GUITAR LESSONS y antes de que te quieras dar cuenta, estarás tocando un gran concierto en tu garito favorito.", "l", "l"));
+        entrada_array.add(new Entrada("GUITARRA ACÚSTICA", "n", "16/03/2021", "Nivel Básico", "Conviértete en un fabuloso guitarrista gracias a nuestro fabuloso método basado en los mejores clickbaits de Youtube. Conocerás todos los secretos de la técnica de los mejores \\\"Room-guitarist\\\" de Youtube. Tocarás como nadie sin salir de tu habitación. \n" +
+                "Olvídate del miedo escénico y de salir de tediosas giras donde no comerás bien y dormirás en un catre. Tus vértebras lo agradecerán! Todo ello por un módico precio de 55 €/mes.", "¿Imaginas dedicarte profesionalmente a la música? Mejor cambia de idea...\n" +
+                "Si todavía sigues leyendo esto es que eres un verdadero apasionado. Cómprate una guitarra y comienza a aprender todo lo necesario para ser un gran profesional en el futuro.\n" +
+                "Tenemos una gran biblioteca de recursos, métodos, programaciones y demás material necesario para que aprendas de la forma más cómoda y personalizada posible.\n" +
+                "Soñar es gratis, pero cuantas más vueltas le des, peor. Lánzate ya a través de BEST ONLINE GUITAR LESSONS y antes de que te quieras dar cuenta, estarás tocando un gran concierto en tu garito favorito.", "b", "b"));
 
         recycler.setHasFixedSize(true);
         GridLayoutManager glm = new GridLayoutManager(getActivity(), 2);
